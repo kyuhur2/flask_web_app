2aff8885b228a4f39d1699f1d5eb5d04114fefe3
kyuhur2 <kyuhur2@gmail.com>
2022-08-14 17:14:23 +0900

    update main

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse, fields, marshal
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 api = Api(app)
 db = SQLAlchemy(app)
 app.config["SQLALCHEMY_DATA_BASE_URI"] = "sqlite:///data/database.db"
 
+@app.route("/")
+def home():
+    return "Flask app."
 
 if __name__ == "__main__":
     app.run()  # omit debug=True later
